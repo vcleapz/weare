c615657a871f4db97e87096648e455610821ee93
arywicaksono <arywicaksono1976@gmail.com>
2019-12-29 11:04:31 +0700

    server.js listen

--- server.js
+++ server.js
@@ -10,5 +10,7 @@ app.use(bodyParser.json());
 var routes = require('./routes');
 routes(app);
 
-app.listen(port);
+
+
+app.listen(process.env.PORT || 5000);
 console.log('CWS API with node js for PCS, RESTful API server started on: ' + port);
