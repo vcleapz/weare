ada0312cc3cbc2b3f3ea6485d28d3b645c765138
arywicaksono <arywicaksono1976@gmail.com>
2019-12-29 12:50:16 +0700

    con js mix

--- conn.js
+++ conn.js
@@ -1,15 +1,59 @@
 var mysql = require('mysql');
+var express = require("express");
+var app = express();
+app.use(express.logger());
 
-var con = mysql.createConnection({
+var db_config = {
     host: "us-cdbr-iron-east-05.cleardb.net",
     user: "b74e0ac591e038",
     password: "8b24d10f",
     database: "heroku_d38d2ba38f5ba8e"
+};
+
+var connection;
+
+function handleDisconnect() {
+    console.log('1. connecting to db:');
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+    // the old one cannot be reused.
+
+    connection.connect(function (err) {              	// The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('2. error when connecting to db:', err);
+            setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
+        }                                     	// to avoid a hot loop, and to allow our node script to
+    });                                     	// process asynchronous requests in the meantime.
+    // If you're also serving http, display a 503 error.
+    connection.on('error', function (err) {
+        console.log('3. db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
+            handleDisconnect();                      	// lost due to either server restart, or a
+        } else {                                      	// connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
+
+app.get('/', function (request, response) {
+    connection.query('SELECT * from t_users', function (err, rows, fields) {
+        if (err) {
+            console.log('error: ', err);
+            throw err;
+        }
+        response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
+    });
 });
 
-con.connect(function (err) {
-    if (err) throw err;
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+    console.log("Listening on " + port);
 });
+
+// con.connect(function (err) {
+//     if (err) throw err;
+// });
 
 
 module.exports = con;
