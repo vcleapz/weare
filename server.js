131f615bb0790f9f59a7048d54fb7d13738c9e3b
arywicaksono <arywicaksono1976@gmail.com>
2019-12-31 16:22:47 +0700

    fix bug#5 asset

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     controller = require('./controller');
 
-app.use('/assets', express.static('public'))
+app.use("/assets", express.static(__dirname + '/assets'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
